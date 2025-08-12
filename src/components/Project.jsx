88ff44702c2cb00a99497b089e3706d5227ba1de
aris kiflan <ariskiflan98@gmail.com>
2025-08-12 15:03:12 +0700

    add new project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -206,6 +206,104 @@ const Project = () => {
                     </td>
                   </tr>
 
+                  <tr className="sr-left border-b border-zinc-300/10 last:border-none">
+                    <td className="py-4 pr-4 align-top text-sm">
+                      <div className="translate-y-px">2024</div>
+                    </td>
+                    <td className="py-4 pr-4 align-top font-semibold leading-snug text-zinc-200">
+                      <div>
+                        <div className="block sm:hidden">
+                          <a
+                            className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-cyan-300 focus-visible:text-cyan-300  sm:hidden group/link text-base"
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            aria-label="Emerson Collective (opens in a new tab)"
+                            href="https://github.com/snykk/microservices-library-management"
+                          >
+                            <span>
+                              Waysfood
+                              <span className="inline-block">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 20 20"
+                                  fill="currentColor"
+                                  className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-0.5"
+                                  aria-hidden="true"
+                                >
+                                  <path
+                                    fill-rule="evenodd"
+                                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                    clip-rule="evenodd"
+                                  ></path>
+                                </svg>
+                              </span>
+                            </span>
+                          </a>
+                        </div>
+                        <div className="hidden sm:block">Waysfood</div>
+                      </div>
+                    </td>
+                    {/* <td className="hidden py-4 pr-4 align-top text-sm lg:table-cell">
+                    <div className="translate-y-px whitespace-nowrap"></div>
+                  </td> */}
+                    <td className="hidden py-4 pr-4 align-top lg:table-cell">
+                      <ul className="flex -translate-y-1.5 flex-wrap">
+                        <li className="my-1 mr-1.5">
+                          <div className="flex items-center rounded-full bg-cyan-400/10 px-3 py-1 text-xs font-medium leading-5 text-cyan-300 ">
+                            Vue.Js
+                          </div>
+                        </li>
+                        <li className="my-1 mr-1.5">
+                          <div className="flex items-center rounded-full bg-cyan-400/10 px-3 py-1 text-xs font-medium leading-5 text-cyan-300 ">
+                            Tailwind
+                          </div>
+                        </li>
+                        <li className="my-1 mr-1.5">
+                          <div className="flex items-center rounded-full bg-cyan-400/10 px-3 py-1 text-xs font-medium leading-5 text-cyan-300 ">
+                            Javascript
+                          </div>
+                        </li>
+                        <li className="my-1 mr-1.5">
+                          <div className="flex items-center rounded-full bg-cyan-400/10 px-3 py-1 text-xs font-medium leading-5 text-cyan-300 ">
+                            Json Placeholder
+                          </div>
+                        </li>
+                      </ul>
+                    </td>
+                    <td className="hidden py-4 align-top sm:table-cell">
+                      <ul className="translate-y-1">
+                        <li className="mb-1 flex items-center">
+                          <a
+                            className="inline-flex items-baseline font-medium leading-tight  hover:text-cyan-300  text-slate-400  focus-visible:text-cyan-300 group/link text-sm"
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            aria-label="emersoncollective.com (opens in a new tab)"
+                            href="https://github.com/ariskiflan/waysfood"
+                          >
+                            <span>
+                              https://github.com/ariskiflan/waysfood
+                              <span className="inline-block">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 20 20"
+                                  fill="currentColor"
+                                  className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-0.5"
+                                  aria-hidden="true"
+                                >
+                                  <path
+                                    fill-rule="evenodd"
+                                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                    clip-rule="evenodd"
+                                  ></path>
+                                </svg>
+                              </span>
+                            </span>
+                          </a>
+                        </li>
+                      </ul>
+                    </td>
+                  </tr>
+
                   <tr className="sr-right border-b border-zinc-300/10 last:border-none">
                     <td className="py-4 pr-4 align-top text-sm">
                       <div className="translate-y-px">2022</div>
